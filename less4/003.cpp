eedd7c45839f3814520e0432c3c9cedb8370e36b
imshef <slahovvsevolod20@gmail.com>
2025-10-16 12:34:21 +0300

    16.10 commit

---
+++ less4/003.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    int num{ 1 };
+
+    while (num <= 100)
+    {
+        std::cout << num << ' ';
+        num++;
+    };
+    std::cout << "\n";
+    
+    return 0;
+    
+}
